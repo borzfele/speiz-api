1fb7ec0a7126e09f338b8066079487a68b917a70
borzfele <borzfele@gmail.com>
2021-05-24 13:59:54 +0200

    rewrote database

--- src/modules/sql/speiz.sql
+++ src/modules/sql/speiz.sql
@@ -1,24 +1,28 @@
 DROP TABLE IF EXISTS shopping_list CASCADE;
+DROP TABLE IF EXISTS status CASCADE;
+
+CREATE TABLE status (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE shopping_list (
   id SERIAL PRIMARY KEY NOT NULL,
-  product VARCHAR(100) NOT NULL,
-  quantity VARCHAR(3) NOT NULL,
-  measurement_unit VARCHAR(50) NOT NULL
+  name VARCHAR(100) NOT NULL,
+  creation TIMESTAMP NOT NULL DEFAULT NOW(),
+  user_id INTEGER NOT NULL DEFAULT 1,
+  status_id INTEGER NOT NULL,
+  CONSTRAINT fk_shopping_list_status_id 
+  FOREIGN KEY(status_id) 
+  REFERENCES status(id)
+  ON DELETE CASCADE
 );
 
-shopping-list
-  id
-  name
-  creation_date
-  status
+INSERT INTO status (name) VALUES ('IN PROGRESS');
+INSERT INTO status (name) VALUES ('DRAFT');
+INSERT INTO status (name) VALUES ('COMPLETED');
 
-product
-  id
-  name
+INSERT INTO shopping_list (name, status_id) VALUES ('Cuki szülinap', 1);
 
-shopping-list-item
-  shopping-list-id
-  product-id
-  quantity
-  measurement_unit
+SELECT * FROM status;
+SELECT * FROM shopping_list;
